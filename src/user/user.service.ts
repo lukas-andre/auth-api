5e4553596d8989a44553d2c54652f93331bcc001
lucasworkmate <lucashenry@workmate.cl>
2020-01-06 18:22:33 -0300

    feature: hash passwords, and remove from responses

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,16 +3,20 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './user.schema';
 import * as mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
 import { UpdateUserDto } from './dto/update-user-dto.interface';
+
+const saltRounds = 10;
 
 @Injectable()
 export class UserService {
     constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
     async createUser(username: string, password: string, email: string) {
+        const cryptPass = bcrypt.hashSync(password, saltRounds);
         const newUser = this.userModel({
             username,
-            password,
+            password: cryptPass,
             email,
         });
         const generatedUser = await newUser.save();
@@ -20,11 +24,10 @@ export class UserService {
     }
 
     async getUsers() {
-        const users = await this.userModel.find().exec();
+        const users = await this.userModel.find({ }, { password: 0 }).exec();
         return users.map(user => ({
             id: user.id,
             username: user.username,
-            password: user.password,
             email: user.email,
         }));
     }
@@ -45,7 +48,8 @@ export class UserService {
             updatedUser.email = updateUserDto.email;
         }
         if (updateUserDto.password) {
-            updatedUser.password = updateUserDto.password;
+            const cryptPass = bcrypt.hashSync(updateUserDto.password, saltRounds);
+            updatedUser.password = cryptPass;
         }
         if (updateUserDto.username) {
             updatedUser.username = updateUserDto.username;
@@ -60,7 +64,7 @@ export class UserService {
         }
         let user;
         try {
-            user = await this.userModel.findById(id).exec();
+            user = await this.userModel.findById(id, { password: 0 }).exec();
         } catch (error) {
             throw new NotFoundException('Could not find user');
         }
