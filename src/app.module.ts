6c36dd9c8416b029bf0372084e79dcd023a0b896
lukas-andre <lucashenry@workmate.cl>
2020-01-08 15:47:51 -0300

    pequeño fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
